ceda5a4f2974370dc3ed2f91402a122dc8aa4111
Dulkith Bandara <115493383+Dulkith13BANdara@users.noreply.github.com>
2024-02-11 13:10:36 +0530

    Update

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php 3cho '
+<div class="footer">
+        <p>
+            Powered by GITHUB
+        </p>
+        <p>
+            Designed By <a href="mailto: dulkithbandara@outlook.com">Dulkith Bandara</a>
+        </p>
+    </div>' ;
+?>
